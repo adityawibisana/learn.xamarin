1e7129788963e0802fff8351d8ba6b7ee33ae2e1
Aditya Wibisana <aditya.wibisana@gmail.com>
2016-09-04 20:06:11 +0700

    Add a navigation sample, from 1 page to another page

---
+++ LearnXamarin/LearnXamarin/LearnXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace LearnXamarin.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void OnGoToNextPageClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
