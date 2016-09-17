ba021e1d2fb0d33a726858b7f8d828ba6ef1c7b9
Aditya Wibisana <aditya.wibisana@gmail.com>
2016-09-17 18:48:00 +0700

    Add static image before first page is loading

---
+++ BlogspotXamarin/BlogspotXamarin/BlogspotXamarin/BlogspotXamarin.Droid/SplashActivity.cs
@@ -0,0 +1,25 @@
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+
+namespace BlogspotXamarin.Droid
+{
+    [Activity(Label = "Aditya Wibisana's", Icon = "@drawable/aw_logo", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, MainLauncher=true, Theme = "@style/MyThemes")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.layout1);  
+            Java.Lang.Runnable runnable = new Java.Lang.Runnable(() =>
+            {
+                Intent i = new Intent(this, typeof(MainActivity));
+                StartActivity(i);
+            });
+
+            new Handler().PostDelayed(runnable, 1500);
+        }
+    }
+}
